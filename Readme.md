3798928a62a54a32d574113b285dac378e39163f
amriths04 <89180140+amriths04@users.noreply.github.com>
2024-09-03 20:10:48 +0530

    connect to mongo atlas

--- Readme.md
+++ Readme.md
@@ -5,3 +5,4 @@ A youtube based project
 -[Model](https://app.eraser.io/workspace/YtPqZ1VogxGy1jzIDkzj)
 
 chai-backend -----> projX
+use .js while importing
--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
-import mongoose, { Mongoose } from "mongoose";
-import DB_NAME from "./constants"
+import dotenv from 'dotenv'
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:'./env'
+})
+
+
+
+connectDB()
+
+
+
+
 
 
 
@@ -13,7 +25,7 @@ const app=express()
 
 ;(async ()=>{
     try {
-        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         app.on("error",(error)=>{
             console.log("ERR:",error)
             throw error
