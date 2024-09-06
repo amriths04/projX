adf24ca4209cc611bbb95e2ea7c9003136b9fb5b
amriths04 <89180140+amriths04@users.noreply.github.com>
2024-09-06 11:53:26 +0530

    file/image upload to cloudinary from local storage

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,34 @@
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
+
+ // Configuration
+ cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_SECRET_KEY // Click 'View API Keys' above to copy your API secret
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath)
+            {
+                return null;
+            }
+        //upload on cloudinary if available
+        const response =await cloudinary.uploader.upload(localFilePath,{
+            resource_type:"auto"
+        })
+        //file uploaded successfully
+        console.log("file is uploaded on cloudinary",response.url)
+        return response
+
+    } catch (error) {
+        //removes the local stored file as uploading failed
+        fs.unlinkSync(localFilePath)
+        return null;
+        
+    }
+}
+   
+    
+export {uploadOnCloudinary}
