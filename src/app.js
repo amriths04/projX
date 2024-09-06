a3d4e8fc6cce9b9d39a7fa91765fe570fee283a9
amriths04 <89180140+amriths04@users.noreply.github.com>
2024-09-06 17:13:00 +0530

    sample api postman code

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express"
-import { cors } from "cors";
-import { cookieParser } from "cookie-parser";
+import  cors  from "cors";
+import cookieParser from 'cookie-parser';  
+
 
 const app =express()
 
@@ -12,15 +13,18 @@ app.use(cors(
 ))
 
 app.use(express.json({limit:"16kb"}))
-
 app.use(express.urlencoded({extended:"true",limit:"16kb"}))
-
 app.use(express.static("public"))
-
 app.use(cookieParser())
 
 
+//routes import 
 
+import userRouter from "./routes/user.routes.js"
+
+//routes declaration
+
+app.use("/api/v1/users",userRouter)
 
 
 export {app}
