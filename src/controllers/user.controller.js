9a09950712aebe581538977554c7f4198ee6df3e
amriths04 <89180140+amriths04@users.noreply.github.com>
2024-09-06 23:16:50 +0530

    partial register validation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,30 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
-
-
+import {ApiError} from "../utils/ApiError.js"
+import {User} from "../models/user.model.js"
 const registerUser =asyncHandler( async  (req,res) =>{
-    return res.status(200).json({
-        message:"HI"
-    })
+   //req.body --json and from
+   const {fullName, email, username, password } = req.body
+   console.log("email: ", email);
+
+//    if(fullName === "")
+//         throw new ApiError(400,"full name is required")
+if (
+    [fullName,email,username,password].some((field) => 
+        field?.trim() === "")
+) {
+    throw new ApiError(400,"All fields are required")
+}
+
+const existedUser =User.findOne({
+    $or:[{ username } , { email }]
+})
+
+if (existedUser) {
+    throw new ApiError(409,"Username and email already existing in database")
+}
+
+req.body
+
 } )
 
 
