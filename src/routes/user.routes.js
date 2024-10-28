c0dcd58817a61365d3089ff0fabef78fef4c5da2
amriths04 <89180140+amriths04@users.noreply.github.com>
2024-10-28 23:23:12 +0530

    small bugs fixed mostly

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -27,7 +27,7 @@ router.route('/change-password').post(verifyJWT,changeCurrentPassword)
 router.route("/current-user").get(verifyJWT,getCurrentUser)
 router.route('/update-details').patch(verifyJWT,updateAccountDetails)
 router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
-router.route("/cover-image").patch(verifyJWT,upload.single("/coverimage"),updateUserCoverImage)
+router.route("/cover-image").patch(verifyJWT,upload.single("coverimage"),updateUserCoverImage)
 
 
 router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
